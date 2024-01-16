407b04aaec1e2a6a9e764f992ebcef9e7b22be78
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-01-16 15:58:02 +0100

    refactor(data|api) Simplify data and api definition

--- api/src/Entity/Employee.php
+++ api/src/Entity/Employee.php
@@ -4,81 +4,31 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
-use ApiPlatform\Metadata\Delete;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Link;
-use ApiPlatform\Metadata\Patch;
-use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Put;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-
-#[ApiResource(
-    operations: [new GetCollection(), new Post(), new Put(), new Patch(), new Delete()],
-    mercure: true
-)]
-#[ApiResource(
-    uriTemplate: '/teams/{teamId}/employees/{id}',
-    uriVariables: [
-        'teamId' => new Link(fromClass: Team::class, toProperty: 'team'),
-        'id' => new Link(fromClass: Employee::class)
-    ],
-    operations: [new Get()],
-    mercure: true
-)]
-#[ApiResource(
-    uriTemplate: '/teams/{teamId}/employees',
-    uriVariables: [
-        'teamId' => new Link(fromClass: Team::class, toProperty: 'team'),
-    ],
-    operations: [new GetCollection()],
-    mercure: true
-)]
 #[ORM\Entity]
+#[ApiResource]
 class Employee
 {
-    /**
-     * The entity ID
-     */
-    #[ORM\Id]
-    #[ORM\Column(type: 'integer')]
-    #[ORM\GeneratedValue]
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
     #[ApiProperty(identifier: true)]
-    private ?int $id = null;
+    public ?int $id = null;
 
-    /**
-     * The entity Name
-     */
     #[ORM\Column]
     #[Assert\NotBlank]
     #[ApiProperty(types: ["https://schema.org/name"])]
     public string $name = '';
 
-    /**
-     * The entity FirstName
-     */
     #[ORM\Column]
     #[Assert\NotBlank]
     public string $firstName = '';
 
-    /**
-     * The entity Position
-     */
     #[ORM\Column]
     #[Assert\NotBlank]
     public string $position = '';
 
-    /**
-     * The employee's associated team
-     */
     #[ORM\ManyToOne(targetEntity: Team::class)]
     #[ORM\JoinColumn(nullable: false)]
     public Team $team;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 }
--- api/src/Entity/Premise.php
+++ api/src/Entity/Premise.php
@@ -7,28 +7,15 @@ use ApiPlatform\Metadata\ApiProperty;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-#[ApiResource(mercure: true)]
 #[ORM\Entity]
+#[ApiResource]
 class Premise
 {
-    /**
-     * The entity ID
-     */
-    #[ORM\Id]
-    #[ORM\Column(type: 'integer')]
-    #[ORM\GeneratedValue]
-    private ?int $id = null;
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+    public ?int $id = null;
 
-    /**
-     * The city name
-     */
     #[ORM\Column]
     #[Assert\NotBlank]
     #[ApiProperty(types: ["https://schema.org/name"])]
     public string $city = '';
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 }
