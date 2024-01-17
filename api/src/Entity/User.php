13d40f39323b2ff3d7296ecac2e40e1219fcb3bd
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-01-17 11:13:15 +0100

    fix(db) Escape table name

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -9,6 +9,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: "`user`")]
 #[ApiResource(security: 'is_granted("ROLE_ADMIN")')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
