aa163d6af4940a7c08bec0d67c8ae4d8560da64d
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-01-17 10:55:04 +0100

    fix(auth) Allow anonymous users to get the entities

--- api/src/Entity/Premise.php
+++ api/src/Entity/Premise.php
@@ -4,15 +4,23 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity]
-#[ApiResource(security: 'is_granted("ROLE_ADMIN")')]
-#[Get(security: 'is_granted("ROLE_USER")')]
-#[GetCollection(security: 'is_granted("ROLE_USER")')]
+#[ApiResource]
+#[Get]
+#[GetCollection]
+#[Delete(security: 'is_granted("ROLE_ADMIN")')]
+#[Patch(security: 'is_granted("ROLE_ADMIN")')]
+#[Post(security: 'is_granted("ROLE_ADMIN")')]
+#[Put(security: 'is_granted("ROLE_ADMIN")')]
 class Premise
 {
     #[ORM\Id, ORM\Column, ORM\GeneratedValue]
